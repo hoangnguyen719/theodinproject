0c633b3f7a256d95d081049f25dfb572423490ca
Hoang Nguyen <hoang.m.nguyen719@gmail.com>
2022-05-13 20:03:21 +0700

    Add basic layout

---
+++ library/js/functions.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function createBookDiv(book) {
+  
+}
